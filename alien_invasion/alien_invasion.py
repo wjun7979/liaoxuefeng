aa0f819a1c0b1fc926c3b8e6c1a3d9a2f2a247e9
wjun7979 <wjun7979@gmail.com>
2020-05-13 10:24:19 +0800

    加入了外星人群

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,17 +14,19 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))  # 设置游戏窗口大小
     pygame.display.set_caption('Alien Invasion')  # 设置窗口标题
-    # 创建一艘飞船
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-    # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)  # 监视键盘和鼠标事件
         ship.update()  # 根据移动标志调整飞船的位置
         gf.update_bullets(bullets)  # 更新子弹的位置，并删除已消失的子弹
-        gf.update_screen(ai_settings, screen, ship, bullets)  # 更新屏幕
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)  # 更新屏幕
 
 
 os.chdir(sys.path[0])  # 将程序运行路径设置成当前文件所在的目录
