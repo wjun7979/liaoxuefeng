81637aae17b9f3efac3c8132dfc7b85bb6314158
wjun7979 <wjun7979@gmail.com>
2020-07-23 08:43:48 +0800

    添加前后端分离实例

---
+++ django/django_vue/frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <Home></Home>
+    </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+    name: "App",
+    components: {
+        Home
+    }
+};
+</script>
+
+<style>
+#app {
+    text-align: center;
+    margin-top: 60px;
+}
+</style>
